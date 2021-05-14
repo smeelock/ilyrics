e6f4a789cab9c647e97c6b3905d8c86426d443d6
Smeelock <aurelien.vungoc@gmail.com>
2021-05-14 17:08:45 +0200

    Add django-heroku dependy

--- website/settings.py
+++ website/settings.py
@@ -18,6 +18,8 @@ import json
 import elasticsearch
 from requests_aws4auth import AWS4Auth # aws authentication
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -181,3 +183,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
