2adb1534e0ae5b1e012423d54d131e510f132210
Smeelock <aurelien.vungoc@gmail.com>
2021-05-14 16:45:39 +0200

    Update host name in settings by removing https

--- website/settings.py
+++ website/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://ilyrics.herokuapp.com"]
+ALLOWED_HOSTS = [ "ilyrics.herokuapp.com" ]
 
 
 # Application definition
