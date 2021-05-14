1a3fe8d74df77ac39919c0fb6e39c99b1c050642
Smeelock <aurelien.vungoc@gmail.com>
2021-05-14 16:38:26 +0200

    Add host ilyrics.herokuapp.com

--- website/settings.py
+++ website/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://ilyrics.herokuapp.com"]
 
 
 # Application definition
