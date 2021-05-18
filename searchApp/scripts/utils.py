cc857e0e0a76c3a6744d83d590253b8a882c208a
Smeelock <aurelien.vungoc@gmail.com>
2021-05-18 15:35:14 +0200

    Update removeDuplicates with multithreading

--- searchApp/scripts/utils.py
+++ searchApp/scripts/utils.py
@@ -18,23 +18,42 @@ def unzip(zip_file, target_dir):
 
 def removeDuplicates():
     """ Removes duplicates. Extremely inefficient because checks for each combination title/artist..."""
+    # find duplicates
     artists = Song.objects.values_list('artist', flat=True).distinct()
-
-    to_delete = []
-    for artist in tqdm(artists, desc="Find duplicates"):
+    print(f"Database info: {Song.objects.all().count()} (total), {len(artists)} (artists)")
+    def _find_duplicates(artist):
+        duplicates = []
         titles = Song.objects.filter(artist=artist).values_list('title', flat=True).distinct()
         for title in titles:
-            duplicates = Song.objects.filter(title=title, artist=artist).values_list('id', flat=True)
-            deletes = Song.objects.filter(pk__in=duplicates[1:])
-            to_delete.append(deletes)
+            ids = Song.objects.filter(title=title, artist=artist).values_list('id', flat=True)
+            d = Song.objects.filter(pk__in=ids[1:])
+            if d.exists():
+                duplicates += list(d)
+        if len(duplicates) > 0:
+            print(f"*Found {len(duplicates)} duplicates for artist {artist}")
+            return duplicates
 
-            # to_delete.delete() # remove duplicates and only keep first one
+    with concurrent.futures.ThreadPoolExecutor(max_workers=WORKERS) as executor:
+        results = list(executor.map(_find_duplicates, artists))
+    
+    to_delete = []
+    for res in results:
+        if res:
+            to_delete += [r.pk for r in res] # store id of songs to delete
+
+    # remove duplicates
     if input(f"Found {len(to_delete)} duplicates, continue and remove them from database? [y/N] ").lower().strip() in ['yes', 'y', 'ok']:
-        for d in tqdm(to_delete, "Delete duplicates"):
-            d.delete() # remove duplicates and only keep first one
-        print(f"Removed {len(to_delete)} duplicates from database!")
+        Song.objects.filter(pk__in=to_delete).delete() # bulk delete duplicates
+        # def _delete_duplicates(d):
+        #     d.delete() # remove duplicates and only keep first one
+        #     print(f"*Removed {len(d)} duplicates")
+        
+        # with concurrent.futures.ThreadPoolExecutor(max_workers=WORKERS) as executor:
+        #     executor.map(_delete_duplicates, to_delete)
+
+        print(f"DONE! Removed {len(to_delete)} duplicates from database!")
     else:
-        print("Aborted.")
+        print("Aborted")
 
 def addSongs(iterator, batch_size=100):
     # def _add_one_song(song):
